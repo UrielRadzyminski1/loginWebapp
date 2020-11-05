6e8da5bcd3205673fbe838ae9c36f55f177cc657
Uriel Radzyminski <uriel@lightit.io>
2020-11-05 14:23:56 -0300

    Start auth process

--- loginApi/routes/api.php
+++ loginApi/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestunitController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,8 @@ use App\Http\Controllers\TestunitController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
 
 Route::post('test/create', [TestunitController::class, 'store']);
